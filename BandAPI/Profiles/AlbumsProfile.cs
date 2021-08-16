2ab5c1144b2239265a739d9a8d0be30762b2663c
Anton Chestikov <chestikov.anton@gmail.com>
2021-08-17 01:33:22 +0500

    Add album get actions.

---
+++ BandAPI/Profiles/AlbumsProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace BandAPI.Profiles
+{
+    public class AlbumsProfile : Profile
+    {
+        public AlbumsProfile()
+        {
+            CreateMap<Entities.Album, Models.AlbumsDto>().ReverseMap();
+        }
+    }
+}
