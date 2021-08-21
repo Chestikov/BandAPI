389c37c1e97c97ebc1dd2aa02e06d22583b034da
Anton Chestikov <chestikov.anton@gmail.com>
2021-08-21 22:21:17 +0500

    Add validation.

--- BandAPI/Models/AlbumForCreatingDto.cs
+++ BandAPI/Models/AlbumForCreatingDto.cs
@@ -1,9 +1,24 @@
-﻿namespace BandAPI.Models
+﻿using BandAPI.ValidationAttributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace BandAPI.Models
 {
-    public class AlbumForCreatingDto
+    [TitleAndDescription(ErrorMessage = "Title must be different from description")] // переопределяется сообщением из TitleAndDescriptionAttribute
+    public class AlbumForCreatingDto //: IValidatableObject  вместо этого испоьзуем TitleAndDescriptionAttribute
     {
+        [Required(ErrorMessage = "Title needs to be filled in")] // переопределяет стандартное сообщение
+        [MaxLength(200, ErrorMessage = "Title needs to be up to 200 characters")]
         public string Title { get; set; }
 
+        [MaxLength(400)]
         public string Description { get; set; }
+
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //    if (Title == Description)
+        //    {
+        //        yield return new ValidationResult("The title and description need to be different", new[] { "AlbumForCreatingDto" });
+        //    }
+        //}
     }
 }
--- BandAPI/Models/BandForCreatingDto.cs
+++ BandAPI/Models/BandForCreatingDto.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace BandAPI.Models
 {
     public class BandForCreatingDto
     {
+        [Required]
+        [MaxLength(100)]
         public string Name { get; set; }
 
         public DateTime Founded { get; set; }
 
+        [Required]
+        [MaxLength(100)]
         public string MainGenre { get; set; }
         public ICollection<AlbumForCreatingDto> Albums { get; set; } = new List<AlbumForCreatingDto>();
 
